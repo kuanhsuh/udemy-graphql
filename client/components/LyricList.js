32a5d5fc6f32a139d0d196d6ff2467d1c401cfcb
dannyhuang <kuanhsuh@gmail.com>
2018-05-02 14:25:31 +0800

    Finish 71

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import { optimistic } from 'apollo-client/optimistic-data/store';
 
 class LyricList extends Component {
-  onLike(id) {
-    console.log(id)
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id }
+    });
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li
-          key={id}
-          className="collection-item"
-          onClick={()=>this.onLike(id)}
-          >
-          {content}
-          <i className="material-icons">thumb_up</i>
+            key={id}
+            className="collection-item"
+            onClick={()=>this.onLike(id, likes)}
+            >
+            {content}
+            <div className="vote-box">
+                <i className="material-icons">thumb_up</i>
+                {likes}
+            </div>
           </li>
       )
     })
@@ -23,10 +31,18 @@ class LyricList extends Component {
     return (
       <ul className="collection">
         {this.renderLyrics()}
-        
       </ul>
     )
   }
 }
 
-export default LyricList
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`
+
+export default graphql(mutation)(LyricList)
